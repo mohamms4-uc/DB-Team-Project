d468c61254344defae23e34febb25b40bbfe7d24
mohamms4-uc <mohamms4@mail.uc.edu>
2024-07-24 00:04:57 -0400

    remake

---
+++ ecommercewebsite/src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import Dashboard from './Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ ecommercewebsite/src/LoginPage.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './LoginPage.css';
+
+const LoginPage = () => {
+    const [users, setUsers] = useState([]);
+    const navigate = useNavigate(); // Define navigate here
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/users')
+            .then(response => {
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error);
+            });
+    }, []);
+
+    const handleUserClick = (user) => {
+        navigate('/dashboard', { state: { userId: user.user_id } });
+    };
+
+    return (
+        <div>
+        <h1 style={{textAlign: 'center', marginTop: '50px'}}>Select a User Profile</h1>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '150px' }}>
+            {users.map(user => (
+                <div
+                    key={user.user_id}
+                    className="user-profile"
+                    onClick={() => handleUserClick(user)}
+                    style={{ margin: '10px', cursor: 'pointer', backgroundColor: 'lightblue' }}
+                >
+                    {/* <img src={`/${user.user_name}.png`} alt={user.user_name} className="profile-img" /> */}
+                    <p>{user.user_name}</p>
+                </div>
+            ))}
+        </div>
+        </div>
+    );
+};
+
+export default LoginPage;
