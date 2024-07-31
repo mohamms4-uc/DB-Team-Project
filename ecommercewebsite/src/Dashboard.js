c7dbb4f7126432a1f47914ca7ee6127b662337c5
mohamms4-uc <mohamms4@mail.uc.edu>
2024-07-30 21:44:47 -0400

    css style add/delete

--- ecommercewebsite/src/Dashboard.js
+++ ecommercewebsite/src/Dashboard.js
@@ -61,6 +61,7 @@ const Dashboard = () => {
         return <p>Loading user data...</p>;
     }
 
+
     return (
         <div className="dashboard-container">
             <Cart />
@@ -69,35 +70,27 @@ const Dashboard = () => {
                 <div className="inner-card">
                     <div className="header-container">
                         <h2>Address Information</h2>
-                        <button className="add-button" onClick={handleAddAddress}>Add</button>
+                        <button className="add-button" onClick={handleAddAddress}>+</button>
                     </div>
                     <div className="address-info">
-                        {userData.addresses && userData.addresses.length > 0 ? (
-                            userData.addresses.map((address) => (
-                                <div key={address.address_id} className="address-item">
-                                    <p>
-                                        {address.street}, {address.city}, {address.state} {address.postal_code} {address.country}
-                                    </p>
-                                    <button 
-                                        className="delete-button" 
-                                        onClick={() => handleDeleteAddress(address.address_id)}
-                                    >
-                                        Delete
-                                    </button>
-                                </div>
-                            ))
-                        ) : (
-                            <p>No addresses available.</p>
-                        )}
+                    <p>{userData.street} {userData.city}, {userData.state} {userData.postal_code} {userData.country}</p>
+                        {userData.addresses.map((address) => (
+                            <div key={address.address_id} className="address-item">
+                                <p>
+                                    {address.street}, {address.city}, {address.state} {address.postal_code} {address.country}
+                                </p>
+                                <button 
+                                    className="delete-button" 
+                                    onClick={() => handleDeleteAddress(address.address_id)}
+                                >
+                                    -
+                                </button>
+                            </div>
+                        ))}
                     </div>
                 </div>
                 <div className="inner-card">
                     <h2>Credit Card Information</h2>
-                    <div className="address-section">
-                        <button className="add-button">Add</button>
-                        <button className="add-button">Delete</button>
-                        <button className="add-button">Edit</button>
-                    </div>
                     <p><strong>Card Number:</strong> {userData.card_number}</p>
                     <p><strong>PIN:</strong> {userData.pin}</p>
                     <p><strong>Expiration Date:</strong> {userData.expiration_date}</p>
